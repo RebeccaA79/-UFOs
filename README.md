20c1d7fbc67606317830862992e6149d3bcf4d32
Rebecca Allison <rebe89@hotmail.com>
2022-06-05 14:15:35 -0400

    final challenge assignment documentation

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
-# -UFOs
+# UFO Sightings Website
+
+## Overview
+
+The purpose of this project was to expand the functionality of the UFO Sightings webpage, which at present only allowed users to search by date. Allowing users to search on multiple fields (such as city, state, country, and shape) at the same time creates a more customizable interface for users to perform an efficient search based on their unique parameters. Javascript was utilized  to display the UFO sightings data in a table format and further tailored by adding filters. Further styling of the HTML page was created by utilizing CSS. 
+
+
+## Results
+
+The below image shows the layout of the UFO Sightings Webpage:
+
+
+Currently there is placeholder information in the various search fields. To perform a search using the city of San Diego as an example, type San Diego ion the search box next to "Enter a City" and hit the Enter button. The table will be filtered on the 3 sightings referencing San Diego. 
+
+To further narrow the search on top of the already filtered data, the user code input 1/1/2010 in the 'Enter a Date" field and hit enter (or tab). The table will update from 3 sightings to 2 sightings. 
+
+To clear the search parameters, simply delete the data entered in the fields and the table will update accordingly back to the original dataset.
+
+
+## Summary
+
+The multi-filter functionality has created a dynamic and user-friendly experience. There are certain limitations and enhancements that can be considered.
+
+One of the drawbacks of this webpage is that the code written in Javascript is not easily reusable. If we wanted to create another page with an additional data table and filters we would have to copy and paste or reuse the same code and tweak it. This could be a tedious process versus using a class or component based structure, which would allow us to reuse our code in multiple places throughout the code in an efficient manner.. 
+
+To address this limitation, the following recommendations for development should be considered:
+
+1. Convert the app.js file into a Javascript class that would accept a list of arguments including a dataset that would allow us to reuse the code throughout the application as needed.
+
+2. Convert this project into a more modern data binding language such as react.js or angular. This would allow us to create reusable components for both our filter feature as well as our data table feature. 
+  
+
+
+
+
+
+
+
+
+
+
+None of the code written is reusable if. 
+
+The summary addresses two additional recommendations for further development (4 pt)
+
+- convert the app.js file into a Javascropt class that would accept a list of arguments including a dataset that would allow us to reuse the code throughout the application as needed.
+
+- Convert this project into a more modern data binding language such as react.js or angular. This would allow us to create reusable components for both our filter feature as well as our data table feature. 
--- static/js/app.js
+++ static/js/app.js
@@ -1,15 +1,20 @@
-// 11.2.4 - import the data from data.js and declare variable:
-const tableData = data;
+// from data.js
+let tableData = data;
+// 1. Create a variable to keep track of all the filters as an object.
+var filters = {};
+// the data we've filtered stored globally
+let filteredData = [];
+// prevFilters
+let prevFilters = {};
 
-// 11.2.4 - Reference the HTML table using d3/ declare a variable:
+// get table references
 var tbody = d3.select("tbody");
 
-// 11.5.1 - create function to build table:
 function buildTable(data) {
-    // First, clear out any existing data
-    tbody.html("");
+  // First, clear out any existing data
+  tbody.html("");
 
-  // 11.5.2 -Next, loop through each object in the data
+  // Next, loop through each object in the data
   // and append a row and cells for each value in the row
   data.forEach((dataRow) => {
     // Append a row to the table body
@@ -20,34 +25,110 @@ function buildTable(data) {
     Object.values(dataRow).forEach((val) => {
       let cell = row.append("td");
       cell.text(val);
-      }
-    );
+    });
   });
 }
 
-// 11.5.4 - build filtered table function:
-function handleClick() {
-    // Grab the datetime value from the filter
-    let date = d3.select("#datetime").property("value");
-    let filteredData = tableData;
+// 3. Use this function to update the filters. 
+function updateFilters() {
   
-     // Check to see if a date was entered and filter the
-    // data using that date.
-    if (date) {
-      // Apply `filter` to the table data to only keep the
-      // rows where the `datetime` value matches the filter value
-      filteredData = filteredData.filter(row => row.datetime === date);
+    // 4a. Save the element that was changed as a variable.
+    let changedElement = d3.select(this);
+    console.log("changedElement: ", changedElement);
+    // 4b. Save the value that was changed as a variable.
+    let elementValue = changedElement.property("value");
+    console.log(elementValue);
+    // 4c. Save the id of the filter that was changed as a variable.
+    let filterID = changedElement.attr("id")
+    console.log(filterID);
+
+    prevFilters = filters;
+
+    // 5. If a filter value was entered then add that filterId and value
+    // to the filters list. Otherwise, clear that filter from the filters object.
+    if (elementValue) {
+      console.log("element value is true!");
+      filters[filterID] = elementValue;
+      console.log("filters: ", filters);
+      console.log("filters[filterID]: ", filters[filterID]);
+      console.log("filterID: ", filterID);
+      console.log("elementValue: ", elementValue);
+    } else {
+      console.log("elementValue is not true!!!");
+      console.log("deleting filter: ", filters[filterID]);
+      delete filters[filterID];
+      console.log("filters: ", filters);
     }
   
-     // Rebuild the table using the filtered data
-    // @NOTE: If no date was entered, then filteredData will
-    // just be the original tableData.
-    buildTable(filteredData);
+    // 6. Call function to apply all filters and rebuild the table
+    filterTable();
+  
   }
   
-  // Attach an event to listen for the form button
-  d3.selectAll("#filter-btn").on("click", handleClick);
+  // 7. Use this function to filter the table when data is entered.
+  function filterTable() {
+
+    // if the user cleared all filter fields or if the previous filter state doesn't match the current filter state
+    // we just reset the filteredData and use the original data set
+    if (Object.keys(filters).length === 0 || (Object.keys(prevFilters).legnth !== Object.keys(filters).length)) {
+      console.log("the user cleared all filters");
+      filteredData = [];
+    }
+
+    // 8. Set the filtered data to the tableData.
+    let originalData = tableData;
+    console.log("currentFilters: ", filters);
+    console.log("originalData: ", originalData);
+    console.log("fitleredData: ", filteredData);
+    // if the filteredData array is empty use the original data set
+    if (filteredData.length == 0) {
+      console.log("filteredData was empty let's use the originalData");
+      filterCurrentData(originalData);
+    // if the filteredData array is not empty, use the filteredData set
+    // for iterative filtering and filtering with a combination of filters
+    } else if (filteredData.length >= 1) {
+      console.log("filters were not all cleared, so let's filter the filteredData");
+      filterCurrentData(filteredData);
+    }
+    console.log("filteredData:", filteredData);
+    // 10. Finally, rebuild the table using the filtered data
+    console.log("Object.keys(filters):", Object.keys(filters));
+    // if the filters object is empty build the table with original data set
+    if (Object.keys(filters).length === 0) {
+      buildTable(originalData);
+    // if the filters object is not empty use the newly filteredData
+    } else if (Object.keys(filters).length >= 1) {
+      buildTable(filteredData);
+    }
+  }
+
+  // filters either the original data set or the data we already filtered previously
+  function filterCurrentData(data) {
+    // 9. Loop through all of the filters and keep any data that
+    // matches the filter values
+    data.forEach((row, index) => {
+      // compares the filter keys to the row keys and pulls out the common keys between them
+      let matchingKeys = Object.keys(row).filter({}.hasOwnProperty.bind(filters));
+      // utilizes the matchingKeys to get an array of the currently selected filter values
+      let selectedFilterValues = matchingKeys.map(k => filters[k]);
+      // utilizes the matchingKeys to get an array of the currently selected row values
+      let selectedRowValues = matchingKeys.map(k => row[k]);
+      console.log("selectedRowValues: ", selectedRowValues);
+      // compares the selectedFilterValues to the selectedRowValues to ensure they are equal
+      if (arrayCompare(selectedFilterValues, selectedRowValues) && arrayCompare(selectedRowValues, selectedFilterValues)) {
+        // if so push the row into the fitleredData array
+        filteredData.push(row);
+      }
+    });
+  }
+  
+  // uses the built in every() method to compare each array value of 2 arrays
+  function arrayCompare(arr1, arr2) {
+    return arr2.every(arr2Item => arr1.includes(arr2Item));
+  }
+  
+  // 2. Attach an event to listen for changes to each filter
+  d3.selectAll("input").on("change", updateFilters);
   
   // Build the table when the page loads
   buildTable(tableData);
-  
